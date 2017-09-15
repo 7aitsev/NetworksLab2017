b579a9cf507d5c4c977a891d0b794c0107ba6442
Maksim Zaitsev <froxton@outlook.com>
2017-09-15 13:48:23 +0400

    Tab to spaces

--- tcp_template/client_win/main.c
+++ tcp_template/client_win/main.c
@@ -11,18 +11,18 @@ error(const char *errMsg, const SOCKET *socket, void (*exit)(int))
     if(0 == FormatMessage(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM,
                           0, (DWORD) WSAGetLastError(), 0, (LPSTR) &decErrCode, 0, 0)
             )
-	{
+    {
         *decErrCode = '\0';
     }
-	fprintf(stderr, ":\n%s %s\n", errMsg, decErrCode);
+    fprintf(stderr, ":\n%s %s\n", errMsg, decErrCode);
     if(socket != NULL)
-	{
-		shutdown(*socket, SD_BOTH);
+    {
+        shutdown(*socket, SD_BOTH);
         closesocket(*socket);
     }
 
     if(exit != NULL)
-	{
+    {
         WSACleanup();
         (*exit)(EXIT_FAILURE);
     }
@@ -47,7 +47,7 @@ readn(SOCKET sfd, char * const data, size_t* dsize)
     }
 
     *dsize = total;
-	data[total] = '\0';
+    data[total] = '\0';
 
     return SOCKET_ERROR == n ? EXIT_FAILURE : EXIT_SUCCESS;
 }
@@ -55,42 +55,42 @@ readn(SOCKET sfd, char * const data, size_t* dsize)
 SOCKET
 prepareclient(char* host, char* port)
 {
-	SOCKET sfd = INVALID_SOCKET;
-	int yes = 1;
+    SOCKET sfd = INVALID_SOCKET;
+    int yes = 1;
     struct addrinfo hints;
-	struct addrinfo* p;
+    struct addrinfo* p;
     struct addrinfo* serverInfo;
 
     memset(&hints, 0, sizeof hints);
     hints.ai_family = AF_INET;
     hints.ai_socktype = SOCK_STREAM;
     hints.ai_protocol = IPPROTO_TCP;
-	
+    
     if(0 != getaddrinfo(host, port, &hints, &serverInfo))
-	{
+    {
         error("getpeername() failed", NULL, exit);
     }
-	
-	for(p = serverInfo; p != NULL; p = p->ai_next)
-	{
+    
+    for(p = serverInfo; p != NULL; p = p->ai_next)
+    {
         sfd = socket(
                 serverInfo->ai_family,
                 serverInfo->ai_socktype,
                 serverInfo->ai_protocol
         );
         if(INVALID_SOCKET == sfd)
-		{
+        {
             continue;
         }
 
         if(0 != setsockopt(sfd, SOL_SOCKET, SO_REUSEADDR, 
-				(char*) &yes, sizeof(yes)))
-		{
+                (char*) &yes, sizeof(yes)))
+        {
             error("setsockopt() failed", NULL, exit);
         }
 
         if(0 != connect(sfd, p->ai_addr, p->ai_addrlen))
-		{
+        {
             error("connect() failed", &sfd, NULL);
             sfd = INVALID_SOCKET;
             continue;
@@ -100,64 +100,64 @@ prepareclient(char* host, char* port)
     freeaddrinfo(serverInfo);
 
     if(INVALID_SOCKET == sfd)
-	{
+    {
         error("Unable to connect to the server!", NULL, exit);
     }
-	
-	return sfd;
+    
+    return sfd;
 }
 
 int
 runclient(SOCKET sfd)
 {
-	const int bufsize = 256;
-	size_t limit = bufsize - 1;
-	char buf[bufsize];
-	size_t inplen;
-	
-	setvbuf(stdout, NULL, _IONBF, 0);
-	printf("Please enter the message: ");
-	buf[limit] = '\0';
+    const int bufsize = 256;
+    size_t limit = bufsize - 1;
+    char buf[bufsize];
+    size_t inplen;
+    
+    setvbuf(stdout, NULL, _IONBF, 0);
+    printf("Please enter the message: ");
+    buf[limit] = '\0';
     fgets(buf, limit, stdin);
-	inplen = strlen(buf);
-	
-	if(SOCKET_ERROR == send(sfd, buf, (int) inplen, 0))
-	{
-		error("send() failed", &sfd, exit);
-	}
-	shutdown(sfd, SD_SEND);
-	
+    inplen = strlen(buf);
+    
+    if(SOCKET_ERROR == send(sfd, buf, (int) inplen, 0))
+    {
+        error("send() failed", &sfd, exit);
+    }
+    shutdown(sfd, SD_SEND);
+    
     if(0 != readn(sfd, buf, &limit))
     {
         fprintf(stderr, "Was read %d bytes, but recv() failed", limit);
-		error("", &sfd, exit);
+        error("", &sfd, exit);
     }
     shutdown(sfd, SD_RECEIVE);
     closesocket(sfd);
-	
-	printf("%s\n", buf);
-	
-	return 0;
+    
+    printf("%s\n", buf);
+    
+    return 0;
 }
 
 int
 main(int argc, char** argv)
 {
-	if(3 > argc)
-	{
+    if(3 > argc)
+    {
         fprintf(stderr, "usage %s hostname port\n", argv[0]);
         exit(EXIT_FAILURE);
     }
-	
-	WSADATA wsaData;
+    
+    WSADATA wsaData;
     if(0 != WSAStartup(MAKEWORD(2,2), &wsaData)) {
-		error("WSAStartup() failed:\n", NULL, exit);
+        error("WSAStartup() failed:\n", NULL, exit);
     }
-	
-	SOCKET sfd = prepareclient(argv[1], argv[2]);
-	int rv = runclient(sfd);
-	
-	WSACleanup();
-	
-	return rv;
+    
+    SOCKET sfd = prepareclient(argv[1], argv[2]);
+    int rv = runclient(sfd);
+    
+    WSACleanup();
+    
+    return rv;
 }
--- tcp_template/server_win/main.c
+++ tcp_template/server_win/main.c
@@ -11,18 +11,18 @@ error(const char *errMsg, const SOCKET *socket, void (*exit)(int))
     if(0 == FormatMessage(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM,
                           0, (DWORD) WSAGetLastError(), 0, (LPSTR) &decErrCode, 0, 0)
             )
-	{
+    {
         *decErrCode = '\0';
     }
-	fprintf(stderr, ":\n%s %s\n", errMsg, decErrCode);
+    fprintf(stderr, ":\n%s %s\n", errMsg, decErrCode);
     if(socket != NULL)
-	{
-		shutdown(*socket, SD_BOTH);
+    {
+        shutdown(*socket, SD_BOTH);
         closesocket(*socket);
     }
 
     if(exit != NULL)
-	{
+    {
         WSACleanup();
         (*exit)(EXIT_FAILURE);
     }
@@ -47,7 +47,7 @@ readn(SOCKET sfd, char * const data, size_t* dsize)
     }
 
     *dsize = total;
-	data[total] = '\0';
+    data[total] = '\0';
 
     return SOCKET_ERROR == n ? EXIT_FAILURE : EXIT_SUCCESS;
 }
@@ -55,99 +55,99 @@ readn(SOCKET sfd, char * const data, size_t* dsize)
 SOCKET
 preparewinserver()
 {
-	SOCKET master = INVALID_SOCKET;
-	
-	int yes = 1;
+    SOCKET master = INVALID_SOCKET;
+    
+    int yes = 1;
     struct addrinfo hints;
     struct addrinfo* serverInfo;
     struct addrinfo* p;
-	
-	memset(&hints, 0, sizeof hints);
+    
+    memset(&hints, 0, sizeof hints);
     hints.ai_family = AF_INET;
     hints.ai_socktype = SOCK_STREAM;
     hints.ai_flags = AI_PASSIVE;
-	hints.ai_protocol = IPPROTO_TCP;
-	
-	if(0 != getaddrinfo(NULL, "5001", &hints, &serverInfo))
-	{
-		error("getaddrinfo() failed", NULL, exit);
-	}
-	
+    hints.ai_protocol = IPPROTO_TCP;
+    
+    if(0 != getaddrinfo(NULL, "5001", &hints, &serverInfo))
+    {
+        error("getaddrinfo() failed", NULL, exit);
+    }
+    
     for(p = serverInfo; NULL != p; p = p->ai_next)
-	{
+    {
         master = socket(
                 serverInfo->ai_family,
                 serverInfo->ai_socktype,
                 serverInfo->ai_protocol
         );
         if(INVALID_SOCKET == master)
-		{
+        {
             continue;
         }
-		        
+                
         if(0 != setsockopt(master, SOL_SOCKET, SO_REUSEADDR,
-				(char*) &yes, sizeof(yes)))
-		{
+                (char*) &yes, sizeof(yes)))
+        {
             error("#setsockopt failed:\n", NULL, exit);
         }
         
         if(0 != bind(master, p->ai_addr, p->ai_addrlen))
-		{
+        {
             error("#bind failed:\n", &master, NULL);
             continue;
         }
         break;
     }
     freeaddrinfo(serverInfo);
-	
+    
     if(NULL == p)
-	{
+    {
         error("bind() failed", NULL, exit);
     }
 
     if(0 != listen(master, 5))
-	{
+    {
         error("#listen failed:\n", &master, exit);
     }
-	
-	return master;
+    
+    return master;
 }
 
 int
 runwinserver(SOCKET master)
 {
-	SOCKET slave;
-	struct sockaddr_storage slaveaddr;
-	int slaveaddrlen = sizeof(slaveaddr);
-	const int bufsize = 256;
-	size_t toread = bufsize - 1;
-	char buf[bufsize];
-	
-	slave = accept(master, (struct sockaddr*) &slaveaddr, &slaveaddrlen);
-	if(INVALID_SOCKET == slave)
-	{
-		error("accept() failed", &master, exit);
-	}
-	shutdown(master, SD_BOTH);
-	closesocket(master);
-	
-	if(0 != readn(slave, buf, &toread))
-	{
-		fprintf(stderr, "Was read %d bytes, but recv() failed", toread);
-		error("", &slave, exit);
-	}
-	shutdown(slave, SD_RECEIVE);
-	
-	printf("Here is the message: %s\n", buf);
-	
-	if(SOCKET_ERROR == send(slave, "I got your message", 18, 0))
-	{
-		error("send() failed", &slave, exit);
-	}
-	shutdown(slave, SD_SEND);
-	
-	closesocket(slave);
-	return 0;
+    SOCKET slave;
+    struct sockaddr_storage slaveaddr;
+    int slaveaddrlen = sizeof(slaveaddr);
+    const int bufsize = 256;
+    size_t toread = bufsize - 1;
+    char buf[bufsize];
+    
+    slave = accept(master, (struct sockaddr*) &slaveaddr, &slaveaddrlen);
+    if(INVALID_SOCKET == slave)
+    {
+        error("accept() failed", &master, exit);
+    }
+    shutdown(master, SD_BOTH);
+    closesocket(master);
+    
+    if(0 != readn(slave, buf, &toread))
+    {
+        fprintf(stderr, "Was read %d bytes, but recv() failed", toread);
+        error("", &slave, exit);
+    }
+    shutdown(slave, SD_RECEIVE);
+    
+    printf("Here is the message: %s\n", buf);
+    
+    if(SOCKET_ERROR == send(slave, "I got your message", 18, 0))
+    {
+        error("send() failed", &slave, exit);
+    }
+    shutdown(slave, SD_SEND);
+    
+    closesocket(slave);
+    return 0;
 }
 
 int
@@ -155,13 +155,13 @@ main(void)
 {
     WSADATA wsaData;
     if(0 != WSAStartup(MAKEWORD(2,2), &wsaData)) {
-		error("WSAStartup() failed:\n", NULL, exit);
+        error("WSAStartup() failed:\n", NULL, exit);
     }
-	
-	SOCKET master = preparewinserver();
-	int rv = runwinserver(master);
-	
-	WSACleanup();
-	
-	return rv;
+    
+    SOCKET master = preparewinserver();
+    int rv = runwinserver(master);
+    
+    WSACleanup();
+    
+    return rv;
 }
