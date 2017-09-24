da7ecb355293acfaac43e952330ac97cc54769d5
Maxim Zaitsev <7aitsev@pm.me>
2017-09-24 21:46:03 +0300

    implemented a dynamic approach to allocating memory for peers

--- src/server/handler/handler.c
+++ src/server/handler/handler.c
@@ -9,14 +9,17 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 
-#define HANDLER_PEERS_LIMIT 25
+#define HANDLER_PEERS_LIMIT 20
 #define HANDLER_PEERS_SIZE 5
 
 static peer_t g_current;
 static peer_t g_total;
 
-static int g_peerslen;
+static peer_t g_peerslen;
 static struct peer* g_peers;
+#define DOUBLE_LENGTH ((g_peerslen << 1) >= HANDLER_PEERS_LIMIT \
+                                         ? HANDLER_PEERS_LIMIT \
+                                         : g_peerslen << 1)
 
 static pthread_mutex_t g_lock;
 
@@ -39,13 +42,13 @@ handler_destroy()
     pthread_mutex_destroy(&g_lock);
 }
 
-int
+peer_t
 handler_getcurrent()
 {
     return __sync_or_and_fetch(&g_current, 0);
 }
 
-int
+peer_t
 handler_gettotal()
 {
     return __sync_or_and_fetch(&g_total, 0);
@@ -54,8 +57,20 @@ handler_gettotal()
 static void*
 handler_test(void* arg)
 {
+    pthread_mutex_lock(&g_lock);
+    struct peer p;
     struct peer* ppeer = (struct peer*) arg;
-    int sfd = ppeer->p_sfd;
+    if(peer_isnotexist(ppeer))
+    {
+        // somehow the peer had been destroyed
+        //  before the thread started
+        pthread_mutex_unlock(&g_lock);
+        return arg;
+    }
+    peer_set(&p, ppeer);
+    pthread_mutex_unlock(&g_lock);
+
+    int sfd = p.p_sfd;
     char buffer[100];
 
     while(1)
@@ -67,14 +82,24 @@ handler_test(void* arg)
             break;
         }
 
-        logger_log("[handler] %d, recv:%s\n", sfd, buffer);
+        logger_log("[test] %d, recv:%s\n", sfd, buffer);
 
         send(sfd, "I got your message", 18, 0);
     }
-    pthread_detach(ppeer->p_tid);
-    peer_destroy(ppeer);
-    __sync_sub_and_fetch(&g_current, 1);
-
+    
+    pthread_detach(p.p_tid);
+    handler_find_first_and_apply(
+            lambda(int, (struct peer* predic)
+                {return p.p_id == predic->p_id;}
+            ),
+            lambda(void, (struct peer* pp)
+                {
+                    logger_log("[test] Deleting #%d: sfd=%d, tid=%u\n",
+                            pp->p_id, pp->p_sfd, pp->p_tid);
+                    __sync_sub_and_fetch(&g_current, 1);
+                    peer_destroy(pp);
+                }
+            ));
     return arg;
 }
 
@@ -122,25 +147,83 @@ deletepeer(struct peer* ppeer)
     peer_destroy(ppeer);
 }
 
+static int
+handler_realloc(peer_t newlen)
+{
+    logger_log("[realloc] newlen=%d\n", newlen);
+    struct peer* newpeers;
+    int actualsize = newlen * sizeof(struct peer);
+    if(NULL != (newpeers = realloc(g_peers, actualsize)))
+    {
+        g_peers = newpeers;
+        if(newlen > g_peerslen)
+            memset(g_peers + g_peerslen, 0,
+                    sizeof(struct peer) * (newlen - g_peerslen));
+        g_peerslen = newlen;
+        return 0;
+    }
+    else
+    {
+        logger_log("[handler] realloc failed\n");
+        return -1;
+    }
+}
+
+static void
+handler_shrink()
+{
+    peer_t curr = handler_getcurrent();
+    if(g_peerslen > HANDLER_PEERS_SIZE && curr <= g_peerslen / 4)
+    {
+        size_t cnt = 0;
+        for(int i = g_peerslen - 1; cnt != g_peerslen / 2; --i)
+        {
+            if(peer_isnotexist(g_peers + i))
+                ++cnt;
+        }
+        if(g_peerslen / 2 == cnt)
+        {
+            logger_log("[handler] shrink from %d to %d\n", g_peerslen, cnt);
+            handler_realloc(cnt);
+        }
+    }
+}
+
 void
 handler_new(int sfd)
 {
     pthread_mutex_lock(&g_lock);
     logger_log("[handler] new peer sfd=%d\n", sfd);
-    int isfound = find_first_and_apply(
-            &peer_isnotexist,
-            lambda(void, (struct peer* p)
-                {
-                    p->p_sfd = sfd;
-                    p->p_id = __sync_add_and_fetch(&g_total, 1);
-                    __sync_add_and_fetch(&g_current, 1);
-                    pthread_create(&p->p_tid, NULL, handler_test, p);
-                })
-        );
-    if(!isfound)
+    handler_shrink();
+    while(1)
     {
-        logger_log("[handler] Reached the peers limit\n");
-        peer_closesocket(sfd);
+        int isfound = find_first_and_apply(
+                &peer_isnotexist,
+                lambda(void, (struct peer* p)
+                    {
+                        p->p_sfd = sfd;
+                        p->p_id = __sync_add_and_fetch(&g_total, 1);
+                        __sync_add_and_fetch(&g_current, 1);
+                        pthread_create(&p->p_tid, NULL, handler_test, p);
+                    })
+            );
+        if(isfound)
+        {
+            break;
+        }
+        else
+        {
+            if(g_peerslen < HANDLER_PEERS_LIMIT)
+            {
+                logger_log("[handler] realloc from %d to %d\n",
+                        g_peerslen, 2 * g_peerslen);
+                if(0 == handler_realloc(DOUBLE_LENGTH))
+                    continue;
+            }
+            logger_log("[handler] Reached the peers limit\n");
+            peer_closesocket(sfd);
+            break;
+        }
     }
     pthread_mutex_unlock(&g_lock);
 }
@@ -170,4 +253,27 @@ handler_foreach(void (*cb)(struct peer* p))
     logger_log("[handler] foreach\n");
     find_all_and_apply(&peer_isexist, cb);
     pthread_mutex_unlock(&g_lock);
+}
+
+int
+handler_find_first_and_apply(int (*predicate)(struct peer* ppeer),
+        void (*consumer)(struct peer* ppeer))
+{
+    int rv;
+    pthread_mutex_lock(&g_lock);
+    rv = find_first_and_apply(predicate, consumer);
+    pthread_mutex_unlock(&g_lock);
+    return rv;
+}
+
+
+int
+handler_fina_all_and_apply(int (*predicate)(struct peer* ppeer),
+        void (*consumer)(struct peer* ppeer))
+{
+    int rv;
+    pthread_mutex_lock(&g_lock);
+    rv = find_all_and_apply(predicate, consumer);
+    pthread_mutex_unlock(&g_lock);
+    return rv;
 }
